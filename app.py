bd44356a0099cac0ca244414050c00c2573bd04a
emarinhod <58037029+emarinhod@users.noreply.github.com>
2023-04-20 22:18:55 -0300

    Change message

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratorio Pipeline DevOps"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
